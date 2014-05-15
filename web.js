8cc3ec741b6a4ff7bde75cd3187af82e3498eb28
Gert-Jan Berghuis <gertjanberghuis@gmail.com>
2014-05-15 23:52:14 +0200

    init

--- web.js
+++ web.js
@@ -10,8 +10,16 @@ app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
-app.get('/wines', wines.findAll);
-app.get('/wines/:id', wines.findById);
+app.configure(function () {
+    app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
+    app.use(express.bodyParser());
+});
+ 
+app.get('/wines', wine.findAll);
+app.get('/wines/:id', wine.findById);
+app.post('/wines', wine.addWine);
+app.put('/wines/:id', wine.updateWine);
+app.delete('/wines/:id', wine.deleteWine);
 
 var port = Number(process.env.PORT || 3000);
 app.listen(port, function() {
