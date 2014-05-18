f491a25b31d65d4a4454fcbff1d891ee665a96a3
Gert-Jan Berghuis <gertjanberghuis@gmail.com>
2014-05-18 21:55:34 +0200

    test

--- routes/wines.js
+++ routes/wines.js
@@ -13,7 +13,20 @@ console.log("test");
     
     
 //});
+
 var mongo = require('mongodb');
+
+var mongoUri = process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/mydb';
+
+mongo.Db.connect(mongoUri, function (err, db) {
+  db.collection('mydocs', function(er, collection) {
+    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+    });
+  });
+});
+
  
 var Server = mongo.Server,
     Db = mongo.Db,
