457bda4a7e25bde13de0f599da3022aea8c7d6a0
Gert-Jan Berghuis <gertjanberghuis@gmail.com>
2014-05-18 22:48:58 +0200

    test

--- routes/wines.js
+++ routes/wines.js
@@ -1,141 +1,96 @@
-//var mongoose = require('mongoose');
-//mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds053148.mongolab.com:53148/<database name>');
-//mongoose.connect('mongodb://localhost:27017/redeenkind');
-
-// Retrieve
-//var MongoClient = require('mongodb').MongoClient;
-
-// Connect to the db
-//MongoClient.connect("mongodb://localhost:27017/winedb", function(err, db) {
-//  if(!err) {
-//    console.log("We are connected");
-//  }
-    
-    
-//});
-    
-var mongo = require('mongodb');
-
-var mongoUri = process.env.MONGOLAB_URI ||
+var MongoClient = require('mongodb').MongoClient;
+ 
+var myCollection;
+var db;
+ 
+function removeDocument(onRemove){
+    myCollection.findAndModify({name: "doduck"}, [], {remove:true}, function(err, object) {
+        if(err)
+            throw err;
+        console.log("document deleted");
+        onRemove();
+    });
+}
+ 
+function findDocument(onFinded){
+    var cursor = myCollection.find({"name" : "doduck", "company.officialName" : "doduck LTD" });
+    cursor.each(function(err, doc) {
+        if(err)
+            throw err;
+        if(doc==null)
+            return;
+ 
+        console.log("document find:");
+        console.log(doc.name);
+        console.log(doc.company.employed);
+        onFinded();
+    });
+}
+ 
+function fieldComplexeUpdateDocument(onUpdate){
+    myCollection.update({name: "doduck"}, {$set: {company: {employed: 10, officialName: "doduck LTD", industries: ["it consulting", "passionate programming"]}}}, {w:1}, function(err) {
+        if(err)
+            throw err;
+        console.log('entry updated');
+        onUpdate();
+    });
+}
+ 
+function fieldUpdateDocument(onUpdate){
+    myCollection.update({name: "doduck"}, {$set: {industry: "France"}}, {w:1}, function(err) {
+        if(err)
+            throw err;
+        console.log('entry updated');
+        onUpdate();
+    });
+}
+ 
+function simpleUpdateDocument(onUpdate){
+    myCollection.update({name: "doduck"}, {name: "doduck", description: "prototype your idea"}, {w:1}, function(err) {
+    if(err)
+        throw err;
+        console.log('entry updated');
+        onUpdate();
+    });
+}
+ 
+function addDocument(onAdded){
+    myCollection.insert({name: "doduck", description: "learn more than everyone"}, function(err, result) {
+        if(err)
+            throw err;
+ 
+        console.log("entry saved");
+        onAdded();
+    });
+}
+ 
+function createConnection(onCreate){
+ var mongoUri = process.env.MONGOLAB_URI ||
   process.env.MONGOHQ_URL ||
-  'mongodb://localhost:27017/winedb';
-
-var collections = ["wines"];
-var db = require("mongojs").connect(mongoUri, collections);
-
-
- 
-//var Server = mongo.Server,
-//    Db = mongo.Db,
-//    BSON = mongo.BSONPure;
- 
-//var server = new Server('localhost', 27017, {auto_reconnect: true});
-//db = new mongo.db('winedb', server);
-db.open(function(err, db) {
-    if(!err) {
-        console.log("Connected to 'winedb' database");
-        db.collection('wines', {strict:true}, function(err, collection) {
-            if (err) {
-                console.log("The 'wines' collection doesn't exist. Creating it with sample data...");
-                populateDB();
-            }
-        });
-    }
+  'mongodb://localhost:27017/redeenkind';
+    
+    MongoClient.connect(mongoUri, function(err, db) {
+        if(err)
+            throw err;
+        console.log("connected to the mongoDB !");
+        myCollection = db.collection('test_collection');
+ 
+        onCreate();
+    });
+}
+ 
+createConnection(function(){
+    addDocument(function(){
+        simpleUpdateDocument(function(){
+            fieldUpdateDocument(function(){
+                fieldComplexeUpdateDocument(function(){
+                    findDocument(function(){
+                        removeDocument(function(){
+                            console.log("The end");
+                        });
+                    });
+                });
+            });
+        });
+    });
 });
- 
-exports.findById = function(req, res) {
-    var id = req.params.id;
-    console.log('Retrieving wine: ' + id);
-    db.collection('wines', function(err, collection) {
-        collection.findOne({'_id':new BSON.ObjectID(id)}, function(err, item) {
-            res.send(item);
-        });
-    });
-};
- 
-exports.findAll = function(req, res) {
-    db.collection('wines', function(err, collection) {
-        collection.find().toArray(function(err, items) {
-            res.send(items);
-        });
-    });
-};
- 
-exports.addWine = function(req, res) {
-    var wine = req.body;
-    console.log('Adding wine: ' + JSON.stringify(wine));
-    db.collection('wines', function(err, collection) {
-        collection.insert(wine, {safe:true}, function(err, result) {
-            if (err) {
-                res.send({'error':'An error has occurred'});
-            } else {
-                console.log('Success: ' + JSON.stringify(result[0]));
-                res.send(result[0]);
-            }
-        });
-    });
-}
- 
-exports.updateWine = function(req, res) {
-    var id = req.params.id;
-    var wine = req.body;
-    console.log('Updating wine: ' + id);
-    console.log(JSON.stringify(wine));
-    db.collection('wines', function(err, collection) {
-        collection.update({'_id':new BSON.ObjectID(id)}, wine, {safe:true}, function(err, result) {
-            if (err) {
-                console.log('Error updating wine: ' + err);
-                res.send({'error':'An error has occurred'});
-            } else {
-                console.log('' + result + ' document(s) updated');
-                res.send(wine);
-            }
-        });
-    });
-}
- 
-exports.deleteWine = function(req, res) {
-    var id = req.params.id;
-    console.log('Deleting wine: ' + id);
-    db.collection('wines', function(err, collection) {
-        collection.remove({'_id':new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
-            if (err) {
-                res.send({'error':'An error has occurred - ' + err});
-            } else {
-                console.log('' + result + ' document(s) deleted');
-                res.send(req.body);
-            }
-        });
-    });
-}
- 
-/*--------------------------------------------------------------------------------------------------------------------*/
-// Populate database with sample data -- Only used once: the first time the application is started.
-// You'd typically not find this code in a real-life app, since the database would already exist.
-var populateDB = function() {
- 
-    var wines = [
-    {
-        name: "CHATEAU DE SAINT COSME",
-        year: "2009",
-        grapes: "Grenache / Syrah",
-        country: "France",
-        region: "Southern Rhone",
-        description: "The aromas of fruit and spice...",
-        picture: "saint_cosme.jpg"
-    },
-    {
-        name: "LAN RIOJA CRIANZA",
-        year: "2006",
-        grapes: "Tempranillo",
-        country: "Spain",
-        region: "Rioja",
-        description: "A resurgence of interest in boutique vineyards...",
-        picture: "lan_rioja.jpg"
-    }];
- 
-    db.collection('wines', function(err, collection) {
-        collection.insert(wines, {safe:true}, function(err, result) {});
-    });
- 
-};
--- web.js
+++ web.js
@@ -26,11 +26,11 @@ var express = require('express'),
 var app = express();
 
  
-app.get('/wines', wine.findAll);
-app.get('/wines/:id', wine.findById);
-app.post('/wines', wine.addWine);
-app.put('/wines/:id', wine.updateWine);
-app.delete('/wines/:id', wine.deleteWine);
+//app.get('/wines', wine.findAll);
+//app.get('/wines/:id', wine.findById);
+//app.post('/wines', wine.addWine);
+//app.put('/wines/:id', wine.updateWine);
+//app.delete('/wines/:id', wine.deleteWine);
  
 app.listen(3000);
 console.log('Listening on port 3000...');
