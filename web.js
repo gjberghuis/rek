bd2b0fc53aa6caeb2fdd188c7bcf321a7e333d98
Gert-Jan Berghuis <gertjanberghuis@gmail.com>
2014-06-25 21:42:35 +0200

    asdf

--- web.js
+++ web.js
@@ -30,7 +30,7 @@ if (req.method === 'OPTIONS') {
       headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
      headers["Access-Control-Allow-Credentials "] = "true"; // 24 hours
       headers["Access-Control-Max-Age"] = '86400'; // 24 hours
-      headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Authorization, Accept";
+      headers["Access-Control-Allow-Headers"] = "*";
       res.writeHead(200, headers);
       res.end();
 } 
@@ -38,7 +38,7 @@ if (req.method === 'OPTIONS') {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');    
         res.header["Access-Control-Max-Age"] = '86400'; // 24 hours
-      res.header["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Authorization";
+      res.header["Access-Control-Allow-Headers"] = "*";
  res.header["Access-Control-Allow-Credentials "] = "true"; // 24 hours
   next();
     }
