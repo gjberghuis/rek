dfb98016c1dbb27ef81ed17a7cc4fb98b4610daf
Gert-Jan Berghuis <gertjanberghuis@gmail.com>
2014-06-25 22:38:46 +0200

    a

--- web.js
+++ web.js
@@ -25,11 +25,11 @@ if (req.method === 'OPTIONS') {
       var headers = {};
       // IE8 does not allow domains to be specified, just the *
       // headers["Access-Control-Allow-Origin"] = req.headers.origin;
-      headers["Access-Control-Allow-Origin"] = "*";
+      headers["Access-Control-Allow-Origin"] = "gjberghuis.nl";
       headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
      headers["Access-Control-Allow-Credentials "] = "true"; // 24 hours
       headers["Access-Control-Max-Age"] = '86400'; // 24 hours
-      headers["Access-Control-Allow-Headers"] = "Authorization";
+      headers["Access-Control-Allow-Headers"] = "X-Requested-With, Authorization";
       res.writeHead(200, headers);
       res.end();
 } 
