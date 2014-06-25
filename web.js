ac1212589cd3cc8f180c6fe3816c1b64855836ba
Gert-Jan Berghuis <gertjanberghuis@gmail.com>
2014-06-25 21:36:42 +0200

    e

--- web.js
+++ web.js
@@ -30,7 +30,7 @@ if (req.method === 'OPTIONS') {
       headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
      headers["Access-Control-Allow-Credentials "] = "true"; // 24 hours
       headers["Access-Control-Max-Age"] = '86400'; // 24 hours
-      headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+      headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Authorization, Accept";
       res.writeHead(200, headers);
       res.end();
 } 
